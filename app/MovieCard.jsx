7ea397822a237e726e93fea75cb98c8e8ff6b6ca
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-15 05:29:03 +0800

    feat: fake score

--- app/MovieCard.jsx
+++ app/MovieCard.jsx
@@ -6,6 +6,8 @@ import Image from 'next/image';
 import { Rating } from '@mui/material';
 
 function MovieCard({ movie }) {
+  const fakeScore = Math.random() * 10;
+
   return (
     <div className={styles.card}>
       <Image src={movie.imgUrl} alt='img' width={100} height={150} />
@@ -19,12 +21,14 @@ function MovieCard({ movie }) {
         >{`${movie.releaseDate} / ${movie.country} / ${movie.genre}`}</p>
         <div className={styles.rating}>
           <Rating
-            value={Math.floor(movie.score / 2)}
+            // value={Math.floor(movie.score / 2)}
+            value={Math.floor(fakeScore / 2)}
             size='small'
             readOnly
             className={styles.stars}
           />
-          <div className={styles.score}>{movie.score}</div>
+          {/* <div className={styles.score}>{movie.score}</div> */}
+          <div className={styles.score}>{fakeScore.toFixed(1)}</div>
         </div>
         <p className={styles.abstract}>{`"${movie.abstractInfo}"`}</p>
         <div className={styles.btns}>
