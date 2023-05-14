f63dd820e08d12ad2ade642e6a8a92a75a350c09
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-15 02:10:47 +0800

    feat: install mui

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import './globals.css';
 import { Roboto } from 'next/font/google';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const roboto = Roboto({ subsets: ['latin'] });
 
