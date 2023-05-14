dd319f0af8c1e78280a7382bd6f3d858902b0657
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-15 02:08:54 +0800

    fix: use roboto font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Douban Movies',
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className={inter.className}>
+    <html lang='en' className={Roboto.className}>
       <body>{children}</body>
     </html>
   );
