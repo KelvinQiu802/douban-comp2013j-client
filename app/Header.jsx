cbae73bb5c26ed471c900947d074853a828a5560
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-15 18:18:06 +0800

    feat: header style

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,10 +1,34 @@
+'use client';
+
 import React from 'react';
 import style from './header.module.css';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 function Header() {
+  const router = useRouter();
+
   return (
     <div className={style.header} id='head'>
-      Douban Movies
+      <div className={style.content}>
+        <div className={style.left} onClick={() => router.push('/')}>
+          <Image
+            src={'/favicon.svg'}
+            width={35}
+            height={35}
+            alt='facivon'
+            className={style.icon}
+          />
+          <h1>Douban</h1>
+        </div>
+
+        <div className={style.right}>
+          <Link href={'/login'}>Login</Link>
+          <span>/</span>
+          <Link href={'/signup'}>Signup</Link>
+        </div>
+      </div>
     </div>
   );
 }
