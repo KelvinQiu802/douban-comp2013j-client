633e3c6999867da0e4983555926a90bfe5e3b7b1
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-16 09:39:16 +0800

    fix: cross origin

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,11 @@ export default function RootLayout({ children }) {
       <head>
         <link rel='icon' href='/favicon.svg' sizes='any' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
         <link
           href='https://fonts.googleapis.com/css2?family=Righteous&display=swap'
           rel='stylesheet'
