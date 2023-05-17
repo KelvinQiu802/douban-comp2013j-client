d29441f22bb827363ede71208a8d04d06f4ad38b
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-18 00:55:19 +0800

    feat: render comments

--- app/movie/[id]/Comments.jsx
+++ app/movie/[id]/Comments.jsx
@@ -1,10 +1,37 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './comments.module.css';
 
+async function getComments(id) {
+  const result = await fetch(
+    `http://localhost:7070/api/comments/movie/${id}`
+  ).then((result) => result.json());
+  return result;
+}
+
 function Comments({ movie, isLogin }) {
-  return <div className={style.content}></div>;
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setComments(await getComments(movie.movieId));
+    })();
+  }, [movie]);
+
+  return (
+    <div className={style.content}>
+      {comments.map((comment) => (
+        <div key={comment.commentId} className={style.comment}>
+          <div className={style.info}>
+            <div className={style.user}>{comment.userName}</div>
+            <div className={style.time}>{comment.time.slice(0, -2)}</div>
+          </div>
+          <p>{comment.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Comments;
