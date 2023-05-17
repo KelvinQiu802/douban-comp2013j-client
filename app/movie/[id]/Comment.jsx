a764c6a8be81af15ebda85e2679907cd1a61b31e
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-18 03:42:43 +0800

    feat: vote for comment

--- app/movie/[id]/Comment.jsx
+++ app/movie/[id]/Comment.jsx
@@ -1,12 +1,107 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import style from './comment.module.css';
+import ChangeHistoryTwoToneIcon from '@mui/icons-material/ChangeHistoryTwoTone';
+import ChangeHistoryIcon from '@mui/icons-material/ChangeHistory';
+
+function hasMyVote(votes, name) {
+  for (let i = 0; i < votes.length; i++) {
+    if (votes[i].userName == name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function getVotes(id) {
+  const result = await fetch(
+    `http://localhost:7070/api/commentvotes/${id}`
+  ).then((result) => result.json());
+  return result;
+}
 
 function Comment({ comment }) {
+  const [votes, setVotes] = useState([]);
+  const [upCount, setUpCount] = useState(0);
+  const [downCount, setDownCount] = useState(0);
+  const [voteForUp, setVoteForUp] = useState(false);
+  const [voteForDown, setVoteForDown] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setVotes(await getVotes(comment.commentId));
+    })();
+  }, [comment]);
+
+  useEffect(() => {
+    setUpCount(votes.filter((vote) => vote.status == 'UP').length);
+    setDownCount(votes.filter((vote) => vote.status == 'DOWN').length);
+    setVoteForUp(
+      hasMyVote(
+        votes.filter((vote) => vote.status == 'UP'),
+        comment.userName
+      )
+    );
+    setVoteForDown(
+      hasMyVote(
+        votes.filter((vote) => vote.status == 'DOWN'),
+        comment.userName
+      )
+    );
+  }, [votes, comment]);
+
   const handleDelete = async (id) => {
     await fetch(`http://localhost:7070/api/comments/${id}`, {
       method: 'DELETE',
     });
     location.reload();
+  };
+
+  const handleUp = async () => {
+    if (!voteForUp && !voteForDown) {
+      // create
+      await fetch(
+        `http://localhost:7070/api/commentvotes/${comment.userName}/${comment.commentId}/UP`,
+        { method: 'POST' }
+      );
+    } else if (!voteForUp && voteForDown) {
+      // update
+      await fetch(
+        `http://localhost:7070/api/commentvotes/${comment.userName}/${comment.commentId}/UP`,
+        { method: 'PUT' }
+      );
+    } else if (voteForUp && !voteForDown) {
+      // delete
+      await fetch(
+        `http://localhost:7070/api/commentvotes/${comment.commentId}`,
+        { method: 'DELETE' }
+      );
+    }
+    setVotes(await getVotes(comment.commentId));
+  };
+
+  const handleDown = async () => {
+    if (!voteForUp && !voteForDown) {
+      // create
+      await fetch(
+        `http://localhost:7070/api/commentvotes/${comment.userName}/${comment.commentId}/DOWN`,
+        { method: 'POST' }
+      );
+    } else if (voteForUp && !voteForDown) {
+      // update
+      await fetch(
+        `http://localhost:7070/api/commentvotes/${comment.userName}/${comment.commentId}/DOWN`,
+        { method: 'PUT' }
+      );
+    } else if (!voteForUp && voteForDown) {
+      // delete
+      await fetch(
+        `http://localhost:7070/api/commentvotes/${comment.commentId}`,
+        { method: 'DELETE' }
+      );
+    }
+    setVotes(await getVotes(comment.commentId));
   };
 
   return (
@@ -24,6 +119,24 @@ function Comment({ comment }) {
         )}
       </div>
       <p>{comment.content}</p>
+      <div className={style.vote}>
+        <div className={style.updiv} onClick={handleUp}>
+          {voteForUp ? (
+            <ChangeHistoryTwoToneIcon className={style.up} fontSize='small' />
+          ) : (
+            <ChangeHistoryIcon className={style.up} fontSize='small' />
+          )}
+          <div>{upCount}</div>
+        </div>
+        <div className={style.downdiv} onClick={handleDown}>
+          {voteForDown ? (
+            <ChangeHistoryTwoToneIcon className={style.down} fontSize='small' />
+          ) : (
+            <ChangeHistoryIcon className={style.down} fontSize='small' />
+          )}
+          <div>{downCount}</div>
+        </div>
+      </div>
     </div>
   );
 }
